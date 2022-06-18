b920ecd24b5dc602dedc13413fc334455a294b39
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-18 16:11:20 +0530

    created starter project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Basics',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: AppHome(),
+    );
+  }
+}
+
+class AppHome extends StatelessWidget {
+  const AppHome({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
